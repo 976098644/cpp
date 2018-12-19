1fae4e55cdd5d6aee4278b98511ec72cdbcf9af9
yxq <976098644@qq.com>
2018-12-19 23:08:56 +0800

    结构体流量f删去

--- vscnm/maxflow.cpp
+++ vscnm/maxflow.cpp
@@ -4,7 +4,6 @@ using namespace std;
 typedef struct {
 	int v;//终点
 	int c;//容量
-	int f;//流
 }edge;
 int *visited;
 int v;
@@ -14,15 +13,7 @@ vector<int> path;
 ifstream infile;
 
 bool bfs();
-
-//void debug() {
-//	for (int i = 0; i < v; i++) {
-//		int l = graph[i].size();
-//		for (int j = 0; j < l; j++)
-//			cout << graph[i].at(j).u << " + " << graph[i].at(j).v << " + " << graph[i].at(j).f << " + " << graph[i].at(j).c << endl;
-//	}
-//
-//}
+vector<edge>::iterator findx(vector<edge> &ut, int vt);
 
 bool bfs() {
 	int i;
@@ -66,13 +57,6 @@ bool bfs() {
 	return false;
 }
 
-edge* finde(int ut, int vt) {
-	int l = graph[ut].size();
-	for (int i = 0; i < l; i++)
-		if (graph[ut].at(i).v == vt)
-			return &graph[ut].at(i);
-	return nullptr;
-}
 vector<edge>::iterator findx(vector<edge> &ut, int vt) {
 	vector<edge>::iterator it;
 	for (it = ut.begin(); it != ut.end(); it++)
@@ -88,29 +72,24 @@ int main() {
 	infile >> v >> e;
 	cout << "point:" << v << endl;
 	cout << "edge:" << e << endl;
-	//int num;
-	//cin >> num;
-	//int shit = 0;
-	//while (shit++ < num) {
-		//cin >> v >> e;
 	visited = new int[v];
 	graph = new vector<edge>[v];
 	edge *edges = new edge[e];
 	int i;
 	//srand((int)time(0));
+
+	//for (i = 0; i < e; i++) {
+	//	edges[i].f = 0;
+	//	edges[i].c = 0;
+	//}
 	for (i = 0; i < e; i++) {
-		edges[i].f = 0;
-		edges[i].c = 0;
-	}
-	for (i = 0; i < e; i++) {
-		/*int u1, v1, c1;
-		cin >> u1 >> v1 >> c1;
-		edges[i].u = u1-1;
-		edges[i].v = v1-1;
-		edges[i].c += c1;*/
 		int ut;
-		infile >> ut >> edges[i].v >> edges[i].c;
-		//edges[i].c = rand();
+		//普通数据
+		//infile >> ut >> edges[i].v >> edges[i].c;
+		//roadNet-CA.txt数据
+		infile >> ut >> edges[i].v;
+		edges[i].c = rand() * 90 + 10;
+
 		vector<edge>::iterator it = findx(graph[ut], edges[i].v);
 		if (it != graph[ut].end())
 			it->c += edges[i].c;
@@ -130,8 +109,7 @@ int main() {
 			ut = path.at(i);
 			vt = path.at(i - 1);
 			it1 = findx(graph[ut], vt);
-			int cf = it1->c - it1->f;
-			min = min < cf ? min : cf;
+			min = min < it1->c ? min : it1->c;
 		}
 		for (i = l - 1; i > 0; i--) {
 			ut = path.at(i);
@@ -141,34 +119,105 @@ int main() {
 			if (it2 == graph[vt].end()) {
 				edge nedge;
 				nedge.v = ut;
-				nedge.c = it1->c;
-				nedge.f = it1->c;
+				nedge.c = 0;
 				graph[vt].push_back(nedge);
 				it2 = graph[vt].end() - 1;
 			}
-			if ((it1->c - it1->f) >= min) {
-				it1->f += min;
-				if (it1->f == it1->c) {
-					graph[ut].erase(it1, it1 + 1);
-				}
-				it2->f -= min;
+			it1->c -= min;
+			if (it1->c == 0) {
+				graph[ut].erase(it1, it1 + 1);
 			}
-			else {
-				it1->f -= min;
-				if (it1->f == 0) {
-					graph[vt].erase(it2, it2 + 1);
-				}
-				it2->f += min;
-			}
+			it2->c += min;
 		}
 		path.clear();
 		result += min;
-		cout << min << endl;
+		cout << "当前流：" << min << endl;
 	}
 	cout << "最大流：" << result << endl;
 	QueryPerformanceCounter(&e);
 	cout << "time:" << (double)(e.QuadPart - s.QuadPart) * 1000 / fre.QuadPart << "ms" << endl;
-	/*	cout << "Case "<<shit<<": " << result << endl;
-	}*/
 	return 0;
 }
+
+//acm
+//int main() {
+//	//infile.open("C:\\Users\\叶学谦\\Desktop\\算法\\roadNet-CA.txt");
+//	//infile.open("C:\\Users\\叶学谦\\Desktop\\算法\\123.txt");
+//	//infile >> v >> e;
+//	//cout << "point:" << v << endl;
+//	//cout << "edge:" << e << endl;
+//	int num;
+//	cin >> num;
+//	int shit = 0;
+//	while (shit++ < num) {
+//		cin >> v >> e;
+//		visited = new int[v];
+//		graph = new vector<edge>[v];
+//		edge *edges = new edge[e];
+//		int i;
+//		//srand((int)time(0));
+//		for (i = 0; i < e; i++) {
+//			edges[i].f = 0;
+//			edges[i].c = 0;
+//		}
+//		for (i = 0; i < e; i++) {
+//			int ut, vt, ct;
+//			cin >> ut >> vt >> ct;
+//			ut--;
+//			edges[i].v = vt-1;
+//			edges[i].c += ct;
+//			//int ut;
+//			//infile >> ut >> edges[i].v >> edges[i].c;
+//			//edges[i].c = rand();
+//			vector<edge>::iterator it = findx(graph[ut], edges[i].v);
+//			if (it != graph[ut].end())
+//				it->c += edges[i].c;
+//			else
+//				graph[ut].push_back(edges[i]);
+//		}
+//		//LARGE_INTEGER s, e, fre;
+//		//QueryPerformanceFrequency(&fre);
+//		//QueryPerformanceCounter(&s);
+//		int result = 0;
+//		while (bfs()) {
+//			int min = INT32_MAX;
+//			int l = path.size(), ut, vt;
+//			vector<edge>::iterator it1;
+//			vector<edge>::iterator it2;
+//			for (i = l - 1; i > 0; i--) {
+//				ut = path.at(i);
+//				vt = path.at(i - 1);
+//				it1 = findx(graph[ut], vt);
+//				int cf = it1->c - it1->f;
+//				min = min < cf ? min : cf;
+//			}
+//			for (i = l - 1; i > 0; i--) {
+//				ut = path.at(i);
+//				vt = path.at(i - 1);
+//				it1 = findx(graph[ut], vt);
+//				it2 = findx(graph[vt], ut);
+//				if (it2 == graph[vt].end()) {
+//					edge nedge;
+//					nedge.v = ut;
+//					nedge.c = it1->c;
+//					nedge.f = it1->c;
+//					graph[vt].push_back(nedge);
+//					it2 = graph[vt].end() - 1;
+//				}
+//				it1->f += min;
+//				if (it1->f == it1->c) {
+//					graph[ut].erase(it1, it1 + 1);
+//				}
+//				it2->f -= min;
+//			}
+//			path.clear();
+//			result += min;
+//			//cout << min << endl;
+//	}
+//	//cout << "最大流：" << result << endl;
+//	//QueryPerformanceCounter(&e);
+//	//cout << "time:" << (double)(e.QuadPart - s.QuadPart) * 1000 / fre.QuadPart << "ms" << endl;
+//		cout << "Case "<<shit<<": " << result << endl;
+//	}
+//	return 0;
+//}
